5469e57bdedfca2f75a1aea35a4ec15143ffe962
LenaSpivakova <Skarlet2710@mail.ru>
2025-05-16 00:48:34 +0300

    Создание таблицы User(ов)

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
+//Создание таблицы User(ов)
+//Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
+//Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
+//Очистка таблицы User(ов)
+//Удаление таблицы
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+
+        userService.saveUser("Alexey", "Sidorov", (byte) 20);
+        userService.saveUser("Maria", "Smirnova", (byte) 23);
+        userService.saveUser("Dmitry", "Suvorov", (byte) 33);
+        userService.saveUser("Petr", "Ivanov", (byte) 25);
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
